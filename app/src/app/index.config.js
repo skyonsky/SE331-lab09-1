b4c47345baa6f67535b50f95843771b559f7bd2c
skyonsky <pranpoom_dis@cmu.ac.th>
2016-04-05 11:41:06 +0700

    1.13 done

--- app/src/app/index.config.js
+++ app/src/app/index.config.js
@@ -8,16 +8,29 @@
     .config(configFlowFactoryProvider);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig) {
-    // Enable log
-    $logProvider.debugEnabled(true);
+  function configTranslation($translateProvider){
+    $translateProvider.useUrlLoader('http://localhost:8080/messageBundle');
+    $translateProvider.useStorage('UrlLanguageStorage');
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.fallbackLanguage('en');
+  }
 
-    // Set options third-party lib
-    toastrConfig.allowHtml = true;
-    toastrConfig.timeOut = 3000;
-    toastrConfig.positionClass = 'toast-top-right';
-    toastrConfig.preventDuplicates = true;
-    toastrConfig.progressBar = true;
+  /** @ngInject */
+  function configFlowFactoryProvider(flowFactoryProvider) {
+    flowFactoryProvider.default = {
+      target :'',
+      permanentErrors:[500, 501],
+      maxChunkRetries:1,
+      chunkRetryInterval:5000,
+      simultaneousUploads :4,
+      singleFile:false
+    };
+
+    /** @ngInject */
+    function configConpilerProvider($compileProvider) {
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|chrome-extension):/);
+      $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data):/);
+    }
   }
 
 })();
